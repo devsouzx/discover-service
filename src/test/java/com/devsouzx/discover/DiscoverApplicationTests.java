18152dad9d9b195c38b89cbe74b9955d3625f8a6
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-10-15 21:49:05 -0300

    first commit

---
+++ src/test/java/com/devsouzx/discover/DiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsouzx.discover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
